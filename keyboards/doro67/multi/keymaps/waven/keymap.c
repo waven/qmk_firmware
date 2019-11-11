8ce02ed1ccfd9070cbaadae1e8c9c1fe0efe9f2d
David Vonau <dvonau@waven.ch>
2019-11-11 23:10:40 +0100

    [Keymap] changed doro67 keymap /waven

--- keyboards/doro67/multi/keymaps/waven/keymap.c
+++ keyboards/doro67/multi/keymaps/waven/keymap.c
@@ -18,21 +18,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "waven.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_DL] = LAYOUT_ansi( \
+  [_DL] = LAYOUT_65_ansi_blocker( \
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,  \
     MO(_CL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,  \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_UP,   MO(_FL),  \
     KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                                      KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
-  [_FL] = LAYOUT_ansi( \
+  [_FL] = LAYOUT_65_ansi_blocker( \
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,   KC_PSCR,  \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MSTP, _______,  \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY, _______,  \
     _______,          _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, KC_VOLU, _______,  \
     _______, _______, _______,          _______,                                     _______, _______,          _______, KC_VOLD, _______  \
   ),
-    [_CL] = LAYOUT_ansi( \
+    [_CL] = LAYOUT_65_ansi_blocker( \
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_F11,    KC_F12,  _______, KC_INS,  \
     _______, _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_MINUS,  _______,   _______, _______, KC_HOME,  \
     _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, _______,            _______, KC_END,  \
