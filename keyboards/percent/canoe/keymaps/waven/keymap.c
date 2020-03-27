ec42e6326ede73565120f8ba5be7f7961ea3bd87
David Vonau <dvonau@waven.ch>
2020-03-27 15:40:49 +0100

    [Keymap] moved canoe keymap /waven

--- keyboards/percent/canoe/keymaps/waven/keymap.c
+++ keyboards/percent/canoe/keymaps/waven/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 David Vonau <dvonau@waven.ch> @waven
+Copyright 2019 @waven
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
