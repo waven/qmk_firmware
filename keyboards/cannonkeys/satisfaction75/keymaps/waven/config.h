8d9e5af261d7f4e41734ac46a0a9bda88f7855cf
David Vonau <dvonau@waven.ch>
2020-03-27 15:42:11 +0100

    [Keymap] adden new statisfaction75 keymap /waven

---
+++ keyboards/cannonkeys/satisfaction75/keymaps/waven/config.h
@@ -0,0 +1,11 @@
+#ifdef ENCODERS_PAD_A
+    #undef ENCODERS_PAD_A
+    #define ENCODERS_PAD_A { B8 }
+#endif
+
+#ifdef ENCODERS_PAD_B
+    #undef ENCODERS_PAD_B
+    #define ENCODERS_PAD_B { B9 }
+#endif
+
+#define ENCODER_RESOLUTION 2
