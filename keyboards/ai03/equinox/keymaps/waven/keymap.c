5385dc804259ea644c62eadcf7d3ed427e8996c8
David Vonau <dvonau@waven.ch>
2020-06-07 00:13:09 +0200

    [Keymap] updated equinox keymap /waven

--- keyboards/ai03/equinox/keymaps/waven/keymap.c
+++ keyboards/ai03/equinox/keymaps/waven/keymap.c
@@ -23,29 +23,35 @@
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all( /* Base */
+  [0] = LAYOUT_all( /* Base normal*/
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT,          MO(2),          KC_SPC,       KC_SPC,        KC_RALT, KC_RGUI, KC_RCTRL
+    KC_LCTL, KC_LGUI, KC_LALT,          LT(3, KC_SPC),  KC_SPC,       KC_SPC,        KC_RALT, KC_RGUI, MO(4)
   ),
-  [1] = LAYOUT_all( /* Extra Keys */
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    _______, KC_F1,   KC_F2,   KC_F2,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12,
-    _______, _______, _______,          MO(3),            _______,     _______,      _______, _______, _______
+  [1] = LAYOUT_all( /* Base gaming*/
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,         LT(3, KC_SPC), LT(3, KC_SPC),KC_RALT, KC_RGUI, MO(4)
   ),
-  [2] = LAYOUT_all( /* Num and FN */
-    KC_ESC,  _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_UP,   KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(3),   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SLSH, _______,
-    _______, _______, _______,          _______,          _______,     _______,      _______, _______, _______
+  [2] = LAYOUT_all(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______, _______, _______, _______, KC_CAPS, _______, _______, _______, _______, _______, _______, KC_F12,
+    _______, _______, _______,          MO(4),            _______,     _______,      _______, _______, KC_RCTL
   ),
-  [3] = LAYOUT_all( /* Num and FN */
-    _______, _______, _______, _______, _______, _______, _______, _______, M_UNCOM, KC_MPRV, KC_MNXT, KC_MSTP,
-    _______, _______, _______, _______, _______, _______, _______, SFT_TAB, M_COM,   KC_TAB,  _______, KC_MPLY,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_VOLU, _______,
-    _______, _______, _______,          _______,          _______,     _______,      _______, _______, _______
+  [3] = LAYOUT_all(
+    KC_GESC, _______, _______, _______, _______, _______, _______, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_BSLS,
+    MO(4),   KC_INS,  KC_PSCR, KC_PAUS, KC_SLCK, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT, TD(TD_PUH),
+    _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______, KC_SLSH, TD(TD_PDE),
+    KC_LGUI, _______, _______,          _______,          _______,     _______,      _______, _______, KC_RCTL
+  ),
+  [4] = LAYOUT_all(
+    RESET,   _______, _______, _______, _______, _______, _______, _______, M_UNCOM, KC_MPRV, KC_MNXT, KC_MSTP,
+    _______, _______, _______, _______, _______, _______,   _______, SFT_TAB, M_COM, KC_TAB,  _______, KC_MPLY,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+    DF(1),   _______, DF(0),            _______,          _______,     _______,      _______, _______, _______
   )
 };
 
