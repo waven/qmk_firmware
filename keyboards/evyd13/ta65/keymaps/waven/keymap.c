0268a313686a22bf5a4a2aadf883490ea5fb3ba9
David Vonau <dvonau@waven.ch>
2020-08-18 23:09:41 +0200

    [Keymap] updated ta65 keymap /waven

--- keyboards/evyd13/ta65/keymaps/waven/keymap.c
+++ keyboards/evyd13/ta65/keymaps/waven/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         MO(_CL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   MO(_FL),
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, _______, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 [_FL] = LAYOUT_65_ansi(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,   KC_PSCR,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_SLCK,
